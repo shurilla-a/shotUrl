b9c67689c73992cadee3cd982f888029193c296e
shurilla <shurilla@gmail.com>
2021-11-17 14:58:58 +0300

    test

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/speps/go-hashids v2.0.0+incompatible // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,29 +5,34 @@ import (
 	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
 	"github.com/speps/go-hashids"
+	_ "gopkg.in/yaml.v2"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
+//структура файла конфигурации
 //type ConfigYaml struct {
-//	Host          string `yaml:"host"`
-//	Port          string `yaml:"port"`
-//	Password      string `yaml:"passwd"`
-//	MaxRetries    int    `yaml:"maxretries"`
-//	DB            int    `yaml:"db"`
-//	KeyLength     int    `yaml:"keylength"`
-//	TtlKey           int    `yaml:"ttl-key"`
+//	Host       string `yaml:"host"`
+//	Port       string `yaml:"port"`
+//	Password   string `yaml:"passwd"`
+//	MaxRetries int    `yaml:"maxretries"`
+//	DB         int    `yaml:"db"`
+//	KeyLength  int    `yaml:"keylength"`
+//	TtlKey     int    `yaml:"ttl-key"`
 //}
 //
-//func ConfigParser(ConfigFile string) (*ConfigYaml, error) {
+//// функция обработки файла конфигурации
+//func ConfigFile(configFile string) (*ConfigYaml, error) {
 //
 //}
+
+// Функция подключения к БД REDIS
 func RedisConnect() *redis.Client {
 	rdbc := redis.NewClient(&redis.Options{
 		DB:         0,
-		Addr:       "172.31.201.78:6379",
+		Addr:       "127.0.0.1:6379",
 		Password:   "",
 		MaxRetries: 5,
 	})
@@ -42,6 +47,7 @@ func RedisConnect() *redis.Client {
 	return rdbc
 }
 
+// Функция Генерации Ключей для связки ключ:значние
 func GenerateKey(rdbc *redis.Client) string {
 	hd := hashids.NewData()
 	hd.MinLength = 7
@@ -66,6 +72,8 @@ func GenerateKey(rdbc *redis.Client) string {
 	return key
 
 }
+
+// Функция Редирект с короткой ссылки на обычную
 func Redirect(w http.ResponseWriter, req *http.Request, rdbc *redis.Client) {
 	params := mux.Vars(req)
 	key := params["key"]
@@ -77,29 +85,37 @@ func Redirect(w http.ResponseWriter, req *http.Request, rdbc *redis.Client) {
 	//defer rdbc.Close()
 }
 
+//Функция создания короткой ссылки
 func Create(w http.ResponseWriter, req *http.Request, rdbc *redis.Client) {
-
 	req.ParseForm()
 	url := req.Form["url"][0]
 	key := GenerateKey(rdbc)
 	_, err := rdbc.Set(key, url, 0).Result()
 	if err != nil {
 		log.Println("НЕ возможно записать ключ "+key+" ошибка ", err)
-		defer rdbc.Close()
-		Create(w, req, rdbc)
+		for i := 0; i < 2; i++ {
+			time.Sleep(1 * time.Minute)
+			RedisConnect()
+		}
+		fmt.Println(returnCode500)
 	}
 	log.Println("Значение по ключу " + key + " Сохранено")
 	fmt.Fprintln(w, "http://127.0.0.1:3128/"+key)
 	//defer rdbc.Close()
 }
 
+//Функция Error 500
+func returnCode500(w http.ResponseWriter, req http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("☄ HTTP status code returned!"))
+}
+
 func main() {
 	logFile, err := os.OpenFile("work.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatalln("Не возможно создать или открыть лог ошибок", err)
+		log.Panicf("Не возможно создать или открыть лог ошибок", err)
 	}
 	log.SetOutput(logFile)
-
 	rdbc := RedisConnect()
 	router := mux.NewRouter()
 	router.HandleFunc("/{key}", func(w http.ResponseWriter, req *http.Request) {
